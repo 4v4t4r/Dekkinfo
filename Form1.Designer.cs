7c90dc048ce869979d38710ed389a30ea7b5e644
Hoaxr <barthezz14@hotmail.com>
2016-08-21 23:27:59 +0200

    DekkInfo
    Show computer info

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace DekkInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textSystemUptime = new System.Windows.Forms.TextBox();
+            this.textExternal = new System.Windows.Forms.TextBox();
+            this.textMemory = new System.Windows.Forms.TextBox();
+            this.textHostname = new System.Windows.Forms.TextBox();
+            this.textCpuLoad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textInternal = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "System Uptime";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hostname";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "CPU Load";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Available Memory";
+            // 
+            // textSystemUptime
+            // 
+            this.textSystemUptime.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textSystemUptime.Location = new System.Drawing.Point(116, 6);
+            this.textSystemUptime.Name = "textSystemUptime";
+            this.textSystemUptime.ReadOnly = true;
+            this.textSystemUptime.Size = new System.Drawing.Size(420, 21);
+            this.textSystemUptime.TabIndex = 7;
+            // 
+            // textExternal
+            // 
+            this.textExternal.Location = new System.Drawing.Point(116, 56);
+            this.textExternal.Name = "textExternal";
+            this.textExternal.ReadOnly = true;
+            this.textExternal.Size = new System.Drawing.Size(420, 21);
+            this.textExternal.TabIndex = 8;
+            this.textExternal.TextChanged += new System.EventHandler(this.textExternal_TextChanged);
+            // 
+            // textMemory
+            // 
+            this.textMemory.Location = new System.Drawing.Point(116, 134);
+            this.textMemory.Name = "textMemory";
+            this.textMemory.ReadOnly = true;
+            this.textMemory.Size = new System.Drawing.Size(420, 21);
+            this.textMemory.TabIndex = 9;
+            // 
+            // textHostname
+            // 
+            this.textHostname.Location = new System.Drawing.Point(116, 30);
+            this.textHostname.Name = "textHostname";
+            this.textHostname.ReadOnly = true;
+            this.textHostname.Size = new System.Drawing.Size(420, 21);
+            this.textHostname.TabIndex = 10;
+            // 
+            // textCpuLoad
+            // 
+            this.textCpuLoad.Location = new System.Drawing.Point(116, 108);
+            this.textCpuLoad.Name = "textCpuLoad";
+            this.textCpuLoad.ReadOnly = true;
+            this.textCpuLoad.Size = new System.Drawing.Size(420, 21);
+            this.textCpuLoad.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "External IP";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Internal IP";
+            // 
+            // textInternal
+            // 
+            this.textInternal.Location = new System.Drawing.Point(116, 82);
+            this.textInternal.Name = "textInternal";
+            this.textInternal.ReadOnly = true;
+            this.textInternal.Size = new System.Drawing.Size(420, 21);
+            this.textInternal.TabIndex = 14;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 163);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(552, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.Stretch = false;
+            this.statusStrip1.TabIndex = 15;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabel1.IsLink = true;
+            this.toolStripStatusLabel1.LinkColor = System.Drawing.Color.Black;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(63, 17);
+            this.toolStripStatusLabel1.Text = "BDekker.nl";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.ForeColor = System.Drawing.Color.Chartreuse;
+            this.toolStripProgressBar1.Margin = new System.Windows.Forms.Padding(373, 3, 1, 3);
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(552, 185);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.textInternal);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textCpuLoad);
+            this.Controls.Add(this.textHostname);
+            this.Controls.Add(this.textMemory);
+            this.Controls.Add(this.textExternal);
+            this.Controls.Add(this.textSystemUptime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DekkInfo";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textSystemUptime;
+        private System.Windows.Forms.TextBox textExternal;
+        private System.Windows.Forms.TextBox textMemory;
+        private System.Windows.Forms.TextBox textHostname;
+        private System.Windows.Forms.TextBox textCpuLoad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textInternal;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Windows.Forms;
+
+namespace DekkInfo
+{
+    public partial class Form1 : Form
+    {
+        // Initalize
+        public Form1()
+        {
+            InitializeComponent();
+
+            textSystemUptime.Text = GetSystemUptime();
+            textHostname.Text = System.Environment.MachineName;
+            textExternal.Text = new WebClient().DownloadString("http://icanhazip.com");
+            textInternal.Text = GetLocalIP();
+            textCpuLoad.Text = GetPerformanceCPU();
+            textMemory.Text = GetPerformanceRam();
+            toolStripProgressBar1.Style = ProgressBarStyle.Marquee;
+
+            // Refresh timer for 1 second
+            timer1 = new Timer { Interval = 1000, Enabled = true };
+            timer1.Tick += new EventHandler(Refresh);
+        }
+
+        public static string GetLocalIP()
+        {
+            // Get local IP address
+            string ipv4Address = String.Empty;
+            foreach (IPAddress currentIPAddress in Dns.GetHostAddresses(Dns.GetHostName()))
+            {
+                if (currentIPAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    ipv4Address = currentIPAddress.ToString();
+                    break;
+                }
+            }
+            // Return ip address
+            return ipv4Address;
+        }
+
+        public static string GetPerformanceCPU()
+        {
+            // Performance counter for CPU
+            PerformanceCounter perfCpuCount = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+
+            // Return CPU value
+            return perfCpuCount.NextValue() + "%";
+        }
+
+        public static string GetPerformanceRam()
+        {
+            // Performance counter for RAM
+            PerformanceCounter perfMemCount = new PerformanceCounter("Memory", "Available MBytes");
+
+            // Return RAM value
+            return perfMemCount.NextValue() + "MB";
+        }
+
+        public static string GetSystemUptime()
+        {
+            // Get the system uptime
+            PerformanceCounter perfupTimeCount = new PerformanceCounter("System", "System Up Time");
+            perfupTimeCount.NextValue();
+            TimeSpan uptimeSpan = TimeSpan.FromSeconds(perfupTimeCount.NextValue());
+
+            // Return uptime
+            return string.Format("{0} Days, {1} Hours, {2} Minutes, {3} Seconds", uptimeSpan.Days, uptimeSpan.Hours, uptimeSpan.Minutes, uptimeSpan.Seconds);
+        }
+
+        private void Refresh(object sender, EventArgs e)
+        {
+            // Fill input fields
+            textSystemUptime.Text = GetSystemUptime();
+            textHostname.Text = System.Environment.MachineName;
+            textExternal.Text = new WebClient().DownloadString("http://icanhazip.com");
+            textInternal.Text = GetLocalIP();
+            textCpuLoad.Text = GetPerformanceCPU();
+            textMemory.Text = GetPerformanceRam();
+            toolStripProgressBar1.Style = ProgressBarStyle.Marquee;
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+            // Visit website
+            System.Diagnostics.Process.Start("http://www.bdekker.nl");
+        }
+
+        private void textExternal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
